8610206bc4a3b057323e5a6100d48b48bbcb7918
martinaapalazzi <martinaapalazzi@gmail.com>
2024-03-06 18:29:43 +0100

    created show.blade.php e aggiunto html

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -10,8 +10,36 @@
                     <h1 class="text-center text-success">
                         All the posts
                     </h1>
-                    <br>
-                    La dashboard è una pagina privata (protetta dal middleware)
+                    
+                    <div>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Slug</th>
+                                    <th scope="col">Created at</th>
+                                    <th scope="col">Show post</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($posts as $post)
+                                    <tr>
+                                        <th scope="row">{{ $post->id }}</th>
+                                        <td>{{ $post->title }}</td>
+                                        <td>{{ $post->slug }}</td>
+                                        <td>{{ $post->created_at }}</td>
+                                        <td>
+                                            <a href="{{ route('admin.posts.show', ['post' => $post->slug]) }}" class="btn btn-xs btn-primary">
+                                                Show
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
                 </div>
             </div>
         </div>
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('page-title', $post->title)
+
+@section('main-content')
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-center text-success">
+                        {{ $post->title }}
+                    </h1>
+                    
+                    <div>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
